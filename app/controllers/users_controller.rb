78ab43dce808eb1c32b00004261c63a9240a0120
Drew Hund <languageinvestigator@gmail.com>
2020-07-29 16:56:15 -0400

    added Users controller and rooted to sign-in page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.permit(:user).require(:name, :email)
+  end
+end
